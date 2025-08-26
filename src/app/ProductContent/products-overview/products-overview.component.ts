20b91d6700347340b051567433dff3d2141d6867
kiransd3 <brks.kiran@gmail.com>
2025-08-26 15:21:14 +0530

    upd12

--- src/app/ProductContent/products-overview/products-overview.component.ts
+++ src/app/ProductContent/products-overview/products-overview.component.ts
@@ -20,7 +20,7 @@ export class ProductsOverviewComponent implements OnInit {
  productList : productLists[] = [
   {prodID : 1, idimg : 'Images/small-tablefan.png', prodClass : '', prodImgName: 'atomberg Aris Contour Smart 1200mm Voice Controlled BLDC Ceiling Fan with Remote| 5 star | Best-in-class Air Flow | Silent Operation', prodprice: 1299, prodOldPrice : 1899, prodWishList : false },
   {prodID : 2, idimg : 'Images/fan9.png', prodClass : '', prodImgName: 'Image FN02', prodprice: 1299, prodOldPrice : 1899, prodWishList : false },
-  {prodID : 3, idimg : 'Images/fan8.png', prodClass : '', prodImgName: 'Image FN03', prodprice: 1299, prodOldPrice : 1899, prodWishList : false},
+  {prodID : 3, idimg : 'Images/fan8.png', prodClass : '', prodImgName: 'atomberg Renesa Elite 1200mm 5-star BLDC Remote Ceiling Fan', prodprice: 1299, prodOldPrice : 1899, prodWishList : false},
   {prodID : 4, idimg : 'Images/ceiling_fan.png', prodClass : '', prodImgName: 'Image FN04', prodprice: 1299, prodOldPrice : 1899, prodWishList : false},
   {prodID : 5, idimg : 'Images/fan7.png', prodClass : '', prodImgName: 'Image FN05', prodprice: 1299, prodOldPrice : 1899, prodWishList : false},
   {prodID : 6, idimg : 'Images/fan10.png', prodClass : '', prodImgName: 'Image FN06', prodprice: 1299, prodOldPrice : 1899, prodWishList : false},
